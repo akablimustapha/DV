2e5e177bc2e4efa5e01c36dff74afc226d0a3d2f
akablimustapha <mustapha.akabli.2018@gmail.com>
2023-05-08 11:12:59 +0200

    sss

--- index.c
+++ index.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
-   printf("hello zorld hhhhhhhhhhhhhhhhhhhhhhhhhh");
+   printf("hello zorld hhhhhhhhhhhhhhhhhhhhhhhhhhxxxxxxxxx");
    return 0;
 }
--- script.c
+++ script.c
@@ -1,7 +1,10 @@
 #include <stdlib.h>
 
 int main() {
-    system("git init");     // Initialise un nouveau référentiel Git
+    system("git init");  
+     system("git config --global init.defaultBranch <main>");  
+       // Initialise un nouveau référentiel Git
+       // Initialise un nouveau référentiel Git
     system("git pull https://github.com/akablimustapha/DV.git main");     // Effectue un "pull" pour récupérer les dernières modifications du référentiel distant
     system("./DV/index");   // Exécute le fichier index.js (assurez-vous que le fichier ait les permissions d'exécution)
     return 0;
