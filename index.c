619a4f47cd4bd9aa0c24ca4f5c8260b529b0e884
akablimustapha <mustapha.akabli.2018@gmail.com>
2023-05-08 11:25:38 +0200

    sss

--- index.c
+++ index.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
-   printf("hello zorld hhhhhhhhhhhhhhhhhhhhhhhhhhxxxxxxxxxHHHHHHHHHHHHHHHHHHHHHHHxxxxxxHHH");
+   printf("hello ");
    return 0;
 }
--- script.c
+++ script.c
@@ -4,14 +4,6 @@
 int main() {
     system("git pull https://github.com/akablimustapha/DV.git main");
 
-    pid_t pid = fork();  // Crée un processus fils
-    if (pid == 0) {  // Si on est dans le processus fils
-        execl("./DV/index", "index", NULL);  // Exécute le fichier index.js
-    }
-    else {  // Si on est dans le processus parent
-        wait(NULL);  // Attend la fin de l'exécution du processus fils
-    }
-
-    return 0;
+   
 }
 
