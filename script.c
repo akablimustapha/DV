e67596b099b861a8474b28d7ae24e38fe10dfc9b
akablimustapha <mustapha.akabli.2018@gmail.com>
2023-05-08 11:28:41 +0200

    sss

--- script.c
+++ script.c
@@ -2,8 +2,9 @@
 #include <unistd.h>
 
 int main() {
-    system("git pull https://github.com/akablimustapha/DV.git main");
-
-   
+    system("git clone https://github.com/akablimustapha/DV.git");
+    system("./DV/index");  // Exécute le fichier index.js (assurez-vous que le fichier a les permissions d'exécution)
+    return 0;
 }
+
 
