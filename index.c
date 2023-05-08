d0c1ae82751a2e07780a03c59c72b9b8f119f242
akablimustapha <mustapha.akabli.2018@gmail.com>
2023-05-08 11:42:33 +0200

    ssklls

--- index.c
+++ index.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
-   printf("hello kkkhhh");
+   printf("hello kkkhhhbbb");
    return 0;
 }
--- script.c
+++ script.c
@@ -4,6 +4,7 @@
 int main() {
         system("git init");
     system("git pull https://github.com/akablimustapha/DV.git");
+    system("git config --global init.defaultBranch main " );
     system("./DV/index");  // Exécute le fichier index.js (assurez-vous que le fichier a les permissions d'exécution)
     return 0;
 }
